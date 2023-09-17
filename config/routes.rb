2f2768e56e040b08b1baef119256cd3f619ffb49
EC2 Default User <ec2-user@ip-172-31-33-245.ap-northeast-1.compute.internal>
2023-09-17 07:44:56 +0000

    controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'public_registrations/new'
+  get 'public_registrations/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
