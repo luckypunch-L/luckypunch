cc8853417e6811656a00c86e4927ff00d6c9f1de
EC2 Default User <ec2-user@ip-172-31-45-122.ap-northeast-1.compute.internal>
2023-09-18 07:57:59 +0000

    public/items

--- db/migrate/20230917054923_create_cart_items.rb
+++ db/migrate/20230917054923_create_cart_items.rb
@@ -2,6 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
 
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,17 +17,7 @@ ActiveRecord::Schema.define(version: 2023_09_17_055012) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "cart_items", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "customers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -50,18 +40,6 @@ ActiveRecord::Schema.define(version: 2023_09_17_055012) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
