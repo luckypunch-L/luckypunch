9c546c9c197a4337f10f17f47db9c4e2e884ce34
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-1.compute.internal>
2023-09-19 09:13:30 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+ 
+  namespace :public do
+    get 'orders/new'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/confirm'
+    get 'orders/conplete'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'items' => 'public/items#index'
   get 'items/:id' => 'public/items#show'
